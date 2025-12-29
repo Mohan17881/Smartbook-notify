e97e4a5840ef62a5395e73033481dd1f252ddcf6
mohan178811 <mohankrishna17856>
2025-12-29 05:49:27 +0530

    version-1 ui-complete

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ emailjs.init({
     publicKey: "9581i2CjsVS8UMlpO"  
 });
 
-document.getElementById("notifyForm").addEventListener("submit", function(e) {
+    const modal = document.getElementById("modal");
+    const closebtn = document.getElementById("closeModal");
+    const form = document.getElementById("notifyForm");
+    form.addEventListener("submit", function(e) {
     e.preventDefault();
     alert(" Your Details Submitted Successfully")
+   
     const email = document.getElementById("email").value;
-    
     const name = document.getElementById("name").value;
+    modal.classList.add("active");
+    form.reset();
+
 
     emailjs.send("service_36bza89", "template_xajtecb", {
         user_email: email,
@@ -22,4 +28,15 @@ document.getElementById("notifyForm").addEventListener("submit", function(e) {
         document.getElementById("msg").innerHTML = "❌ Failed to send email.";
         console.log(error);
     });
+});
+
+closebtn.addEventListener("click" ,()=>{
+    modal.classList.remove("active");
+});
+
+modal.addEventListener('click', (e)=>{
+    if(e.target === modal){
+        modal.classList.remove("active");
+
+    }
 });
